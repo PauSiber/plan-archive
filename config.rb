203e1b3ba77d3c48eb1e3fd1cca228b60cec9a2c
hasantezcan <hasantezcan77@gmail.com>
2020-01-15 22:59:52 +0300

    Code refactor

--- config.rb
+++ config.rb
@@ -6,6 +6,25 @@ activate :autoprefixer do |prefix|
 end
 
 activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+  # blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
 
   # Settings
   blog.permalink = "{title}.html"
